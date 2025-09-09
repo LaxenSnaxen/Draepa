c50bcfe23c926ff3b66e082ac32c6b53881863ce
GalnaRobin <robin.dahl@elev.ntig>
2025-09-09 22:33:04 +0200

    Skapade header fil för sfär fysik (jag hoppas alla projektiler är sfärer annars blir matten mer komplicerad). Skrev grundläggande kod för fysiken. Behöver kollas över så att jag inte gjort några misstag.

--- Physics/physics.cpp
+++ Physics/physics.cpp
@@ -3,3 +3,9 @@
 Koden kommer inte vara kopplad till spelet tills jag eller någon annan fattar hur man gör det
 Kommer försöka hålla variablerna förståbara
 */
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+#include "physics.h"
---
+++ Physics/physics.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+class physicalSphericalObject {
+
+public:
+    
+
+    physicalObject(float radius, float x, float y, float z, float mass, float bounce = 0.5f, float friction = 0.5f)
+        : radius(radius), x(x), y(y), z(z), vx(0), vy(0), vz(vz), ax(0), ay(0), az(az), mass(mass), restitution(restitution), friction(friction) {}
+
+    //Get för alla variabler
+    float radius() const { return radius; }
+    float getPosition() const { return std::make_tuple(x, y, z); }
+    float getVelocity() const { return std::make_tuple(vx, vy, vz); }
+    float getAcceleration() const { return std::make_tuple(ax, ay, az); }
+    float getMass() const { return mass; }
+    float getBounce() const { return bounce; }
+    float getFriction() const { return friction; }
+
+    void airResistance(const float dragCoefficient, const float airDensity) {
+        //Måste prata med Mikael om hur luft motstånd fungerar och ska räknas ut
+
+        //Drag coefficient = 0.45
+    };
+
+    void applyGravity(){
+        ay = -9.81f; // Gravity acceleration in m/s^2
+    };
+
+    void disableGravity(){
+        ay += 9.81f; // lägger till 9.81 för att neutralisera gravitationen (sätter den inte till 0 direkt för att undvika problem med andra krafter)
+    };
+
+    void applyForce(float fx, float fy, float fz) {
+        ax += fx / mass; // F = ma vilket betyder att a = F/m
+        ay += fy / mass;
+        az += fz / mass;
+    };
+
+    void update(float deltaTime) {
+        // updatera hastigheten baserat på acceleration
+        vx += ax * deltaTime;
+        vy += ay * deltaTime;
+        vz += az * deltaTime;
+
+        // updatera positionen baserat på hastighet
+        x += vx * deltaTime;
+        y += vy * deltaTime;
+        z += vz * deltaTime;
+
+        // reseta acceleration efter ny tick
+        ax = 0;
+        ay = 0;
+        az = 0;
+    };
+
+    float getTotalEnergy() const {
+        float kineticEnergy = 0.5f * mass * (vx * vx + vy * vy + vz * vz);
+        float potentialEnergy = mass * 9.81f * y; // om y är höjden från marken annars får en variabel eller konstant läggas till för marknivån 
+        return kineticEnergy + potentialEnergy;
+    }
+
+private:
+    float radius; // Radius of the spherical object
+    float x, y, z; // Position
+    float vx, vy, vz; // Velocity
+    float ax, ay, vz; // Acceleration
+    float mass; // Mass
+    float bounce; // Coefficient of restitution (bounciness)
+    float friction; // Coefficient of friction
+    
+};
